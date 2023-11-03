dd407d64a74f3b7fcdf6fae14b683615889f467e
Skyler James <15920401+skyjay1@users.noreply.github.com>
2023-11-03 11:08:28 -0600

    Clean up copyright headers

--- src/main/java/axolootl/util/ShapeUtils.java
+++ src/main/java/axolootl/util/ShapeUtils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023 XFactHD
+ * Copyright (c) 2023 Skyler James
  * Permission is granted to use, modify, and redistribute this software, in parts or in whole,
  * under the GNU LGPLv3 license (https://www.gnu.org/licenses/lgpl-3.0.en.html)
  **/
